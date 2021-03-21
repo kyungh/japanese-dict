4ba867890a3edfa21d3fc4e642eed4a711836a7c
Kyung Haug <kyung_haug@yahoo.com>
2021-03-20 23:18:23 -0700

    Added words

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace japanese_dict
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (wordBox.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a word!");
+            }
+
+            string x = wordBox.SelectedItem.ToString(); //Converts items in list to string
+            engWord.Content = jpndict.main_Dict(x); //Changes content in label to show JPN hiragana and romaji
+        }
+    }
+}
---
+++ jpndict.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace japanese_dict
+{
+    public class jpndict
+    {
+        public static string JsonConvert { get; private set; }
+
+        public static string main_Dict(string x)
+        {
+            //Below is the dictionary for all the current words in the program/list.
+            string fill = "System.Windows.Controls.ListBoxItem: ";
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add(fill + "hello", "Hiragana: こんにちは \nRomaji: Kon-ni-chi-wa" );
+            dict.Add(fill + "goodbye", "Hiragana: さよなら \nRomaji: Sa-yo-na-ra");
+            dict.Add(fill + "good night", "Hiragana: おやすみなさい \nRomaji: O-ya-su-mi-na-sai");
+            dict.Add(fill + "good morning", "Hiragana: おはようございます \nRomaji: O-ha-you-go-sai-ma-su");
+            dict.Add(fill + "thank you for the meal", "Hiragana: いただきます \nRomaji: I-ta-da-ki-ma-su");
+            dict.Add(fill + "good evening", "Hiragana: こんばんは \nRomaji: Kon-ban-wa");
+
+            //Simple if/else statement to check if highlighted word in list is in the dictionary(all words highlighted should be in dict.
+            if (dict.ContainsKey(x))
+            {
+                return dict[x];
+            }
+            else
+            {
+                return "What are you doing?";
+            }
+        }
+    }
+}
